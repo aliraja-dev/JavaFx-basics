6f335b84418f2dc8b50f6ce8dd936b72a014e02a
Ali Raja <ali@alis-mac-studio.mynetworksettings.com>
2022-05-09 11:16:22 -0400

    added: try-catch for start method JavaFx & scene2 package

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,17 +10,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("JavaFx Project");
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
-        // When we have multiple scenes, we can add them the same CSS file or separate
-        // css files by storing the css =
-        // this.getClass().getResource("main.css").toExternalForm(); in a string and
-        // then reusing that for every scene in getstylesheets().add(css);
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+            primaryStage.setTitle("JavaFx Project");
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
+            // When we have multiple scenes, we can add them the same CSS file or separate
+            // css files by storing the css =
+            // this.getClass().getResource("main.css").toExternalForm(); in a string and
+            // then reusing that for every scene in getstylesheets().add(css);
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
