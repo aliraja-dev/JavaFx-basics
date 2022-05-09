a4417056c8a4da0af9880860af9c371f68abfc9e
Ali Raja <ali@alis-mac-studio.mynetworksettings.com>
2022-05-09 13:04:29 -0400

    added: Exit/close the app

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -22,6 +24,11 @@ public class Main extends Application {
 
             primaryStage.setScene(scene);
             primaryStage.show();
+            // TODO: Lambda functions in Java, arrow functions in JS
+            primaryStage.setOnCloseRequest(event -> {
+                event.consume();
+                exit(primaryStage);
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -29,5 +36,19 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void exit(Stage stage) {
+        // Add alert box
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Are you sure you want to exit?");
+        alert.setContentText("Press OK to exit.");
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            System.out.println("Successfully logged out!");
+            stage.close();
+        }
+        // System.exit(0);
     }
 }
--- src/main/Scene2Controller.java
+++ src/main/Scene2Controller.java
@@ -5,13 +5,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
 public class Scene2Controller {
     @FXML
-    Label InputLabel;
+    private Label InputLabel;
+    @FXML
+    private Button exitBtn;
 
     private Stage stage;
     private Scene scene;
@@ -31,5 +36,20 @@ public class Scene2Controller {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void exit(ActionEvent event) {
+        // Add alert box
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Are you sure you want to exit?");
+        alert.setContentText("Press OK to exit.");
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            System.out.println("Successfully logged out!");
+            stage.close();
+        }
+        // System.exit(0);
     }
 }
